4508874ee698bf84f275ab34e706f32cbce344a1
Akash Perera <akashennn@gmail.com>
2022-05-18 11:49:45 +0800

    Create TransactionDetails.tsx

---
+++ src/pages/TransactionDetails.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TransactionDetailsPage = (): JSX.Element => {
+  return (
+    <div>
+      <p>TransactionDetailsPage</p>
+    </div>
+  );
+};
+
+export default TransactionDetailsPage;
