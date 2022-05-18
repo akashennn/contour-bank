29dad947591e5e8265454eb78438db4392cf7759
Akash Perera <akashennn@gmail.com>
2022-05-18 12:16:25 +0800

    Update _Error.tsx

--- src/pages/_Error.tsx
+++ src/pages/_Error.tsx
@@ -1,14 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import ErrorImage from "../assets/404.svg";
 
 const ErrorPage = (): JSX.Element => {
   return (
-    <div>
-      <img src={ErrorImage} className="error" alt="404" />
-
-      <p>Page Not Found</p>
-    </div>
+    <Container>
+      <img src={ErrorImage} className="error-image" alt="404" />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  .error-image {
+    @media only screen and (min-width: 768px) {
+      /* tablets and desktop */
+      width: 500px;
+      height: 500px;
+    }
+
+    @media only screen and (max-width: 767px) {
+      /* phones */
+      width: 300px;
+      height: 300px;
+    }
+  }
+`;
 
 export default ErrorPage;
