5f25c8071dfc8452c4d37af1c85a028f204941bb
Akash Perera <akashennn@gmail.com>
2022-05-18 13:46:29 +0800

    Update AccountListing.tsx

--- src/pages/AccountListing.tsx
+++ src/pages/AccountListing.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Col, Row } from "react-bootstrap";
+import styled from "styled-components";
+import AccountCard from "../components/Accounts/AccountCard";
+import { AppContext } from "../contexts/AppContext";
 
 const AccountListingPage = (): JSX.Element => {
+  // ctx
+  const { accountsData } = useContext(AppContext);
+
   return (
-    <div>
-      <p>AccountListingPage</p>
-    </div>
+    <Container>
+      <Row xs={1} md={2} className="g-4">
+        {accountsData.map((accountData) => (
+          <Col>
+            <AccountCard accountData={accountData} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-x: scroll;
+`;
 
 export default AccountListingPage;
