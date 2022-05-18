5ea0b22dd5105e06904bfd0da736a6b60d10d992
Akash Perera <akashennn@gmail.com>
2022-05-18 14:48:11 +0800

    Update TransactionListing.tsx

--- src/pages/TransactionListing.tsx
+++ src/pages/TransactionListing.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import TransactionCard from "../components/Transactions/TransactionCard";
+import { AppContext } from "../contexts/AppContext";
 
 const TransactionListingPage = (): JSX.Element => {
+  // ctx
+  const { transactionsData } = useContext(AppContext);
+
+  // router
+  const { accountId } = useParams();
+  const cleanAccountId = parseInt(accountId ?? "0");
+
+  // remove other account transactions
+  const ACCOUNT_TRANSACTIONS_DATA = [
+    ...transactionsData.filter(
+      (transactionData) => transactionData.account_id === cleanAccountId
+    ),
+  ];
+
   return (
-    <div>
-      <p>TransactionListingPage</p>
-    </div>
+    <Container>
+      <Row xs={1} md={1} className="g-4">
+        {ACCOUNT_TRANSACTIONS_DATA.map((transactionData) => (
+          <Col key={transactionData.id}>
+            <TransactionCard transactionData={transactionData} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-x: scroll;
+`;
 
 export default TransactionListingPage;
